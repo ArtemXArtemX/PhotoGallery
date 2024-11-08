1bc7faa9c8ea8ba99b4282e5570442ecedfe8be4
tematrkv <tatarnikov.stud@mail.ru>
2024-11-08 22:13:56 +0700

    7.4 start commit (Создание класса Worker)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx.v100)
     implementation(libs.gson)
+    implementation(libs.android.work.runtime)
     implementation(libs.converter.gson)
     implementation(libs.androidx.lifecycle.extensions)
     implementation(libs.converter.scalars)
